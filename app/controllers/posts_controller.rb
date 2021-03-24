44b0d5e81d9a152538003f0252133098a69f9915
Kevin Pecci <s1191114@monmouth.edu>
2021-03-24 19:21:23 -0400

    Started to add user post association but currently an error happens when trying to load the user's email

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+
     def index
         @posts = Post.all.order("created_at DESC")
     end
 
     def new
-        @post = Post.new
+        @post = current_user.posts.build
     end
 
     def show
@@ -12,12 +15,12 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to @post
-            else
-                render 'new'
-            end
+        @post = current_user.posts.build(post_params)
+    if @post.save
+        redirect_to @post
+        else
+            render 'new'
+        end
     end
 
     def edit
@@ -42,8 +45,6 @@ class PostsController < ApplicationController
     def post_params
         params.require(:post).permit(:title, :content)
     end
-
-    before_action :find_post, only: [:show, :edit, :update, :destroy]
 
     def find_post
         @post = Post.find(params[:id])
